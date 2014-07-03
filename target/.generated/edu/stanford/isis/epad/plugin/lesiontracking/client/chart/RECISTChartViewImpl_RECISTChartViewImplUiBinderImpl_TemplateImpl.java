eafd7a66f1d0387e0c01b9ef64a352f6e1106e9d
francolamping <francolamping@gmail.com>
2014-07-03 15:37:56 -0700

    Cookies Working

--- target/.generated/edu/stanford/isis/epad/plugin/lesiontracking/client/chart/RECISTChartViewImpl_RECISTChartViewImplUiBinderImpl_TemplateImpl.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.isis.epad.plugin.lesiontracking.client.chart;
-
-public class RECISTChartViewImpl_RECISTChartViewImplUiBinderImpl_TemplateImpl implements edu.stanford.isis.epad.plugin.lesiontracking.client.chart.RECISTChartViewImpl_RECISTChartViewImplUiBinderImpl.Template {
-  
-  public com.google.gwt.safehtml.shared.SafeHtml html1() {
-    StringBuilder sb = new java.lang.StringBuilder();
-return new com.google.gwt.safehtml.shared.OnlyToBeUsedInGeneratedCodeStringBlessedAsSafeHtml(sb.toString());
-}
-}
